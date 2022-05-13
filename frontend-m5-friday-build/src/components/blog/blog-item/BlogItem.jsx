2aaa95da39c047e4768c81476c37cd1a59b749df
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-05-13 15:46:16 +0100

    update

--- frontend-m5-friday-build/src/components/blog/blog-item/BlogItem.jsx
+++ frontend-m5-friday-build/src/components/blog/blog-item/BlogItem.jsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 import BlogAuthor from "../blog-author/BlogAuthor";
 import "./styles.css";
 const BlogItem = (props) => {
-  const { title, cover, author, _id } = props;
+  /*   const { title, cover, author, _id } = props; */
   return (
-    <Link to={`/blog/${_id}`} className="blog-link">
+    <Link to={`/blog/${props.id}`} className="blog-link">
       <Card className="blog-card">
-        <Card.Img variant="top" src={cover} className="blog-cover" />
+        <Card.Img variant="top" src={props.imageUrl} className="blog-cover" />
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title>{props.name}</Card.Title>
         </Card.Body>
         <Card.Footer>
-          <BlogAuthor {...author} />
+          <BlogAuthor /* {props.name } */ />
         </Card.Footer>
       </Card>
     </Link>
