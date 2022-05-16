3c8318cb954c6f054565aa179bb4facf9b1ec56a
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-05-16 15:21:19 +0100

    Submitting products, and comments working

--- frontend-m5-friday-build/src/components/navbar/BlogNavbar.jsx
+++ frontend-m5-friday-build/src/components/navbar/BlogNavbar.jsx
@@ -27,7 +27,7 @@ const NavBar = (props) => {
           >
             <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
           </svg>
-          Post Article
+          Post Product
         </Button>
       </Container>
     </Navbar>
--- frontend-m5-friday-build/src/views/new/New.jsx
+++ frontend-m5-friday-build/src/views/new/New.jsx
@@ -4,33 +4,107 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./styles.css";
 const NewBlogPost = (props) => {
-  const [text, setText] = useState("");
-  const handleChange = useCallback((value) => {
-    setText(value);
-  });
+  const [eComProducts, setEComProducts] = useState([]);
+
+  let handleChange = (propertyName, targetValue) => {
+    setEComProducts({
+      ...eComProducts,
+      reviews: [],
+      [propertyName]: targetValue,
+    });
+  };
+
+  let fetchData = async () => {
+    let response = await fetch("http://localhost:5001/products/", {
+      method: "POST",
+      body: JSON.stringify(eComProducts),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      console.log(response);
+    }
+  };
+
+  let submitData = (event) => {
+    event.preventDefault();
+    fetchData();
+  };
+
   return (
     <Container className="new-blog-container">
       <Form className="mt-5">
         <Form.Group controlId="blog-form" className="mt-3">
-          <Form.Label>Title</Form.Label>
-          <Form.Control size="lg" placeholder="Title" />
+          <Form.Label>Name of product</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="Enter the name of the product"
+            onChange={(event) => {
+              handleChange("name", event.target.value);
+            }}
+            value={eComProducts.name}
+          />
+        </Form.Group>
+        <Form.Group controlId="blog-form" className="mt-3">
+          <Form.Label>Product Brand</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="Enter the brand"
+            onChange={(event) => {
+              handleChange("brand", event.target.value);
+            }}
+            value={eComProducts.brand}
+          />
+        </Form.Group>
+        <Form.Group controlId="blog-form" className="mt-3">
+          <Form.Label>Image URL</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="Enter an image URL"
+            onChange={(event) => {
+              handleChange("imageUrl", event.target.value);
+            }}
+            value={eComProducts.imageUrl}
+          />
+        </Form.Group>
+        <Form.Group controlId="blog-form" className="mt-3">
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            size="lg"
+            placeholder="Enter the price"
+            onChange={(event) => {
+              handleChange("price", event.target.value);
+            }}
+            value={eComProducts.price}
+          />
         </Form.Group>
         <Form.Group controlId="blog-category" className="mt-3">
           <Form.Label>Category</Form.Label>
-          <Form.Control size="lg" as="select">
-            <option>Category1</option>
-            <option>Category2</option>
-            <option>Category3</option>
-            <option>Category4</option>
-            <option>Category5</option>
+          <Form.Control
+            size="lg"
+            as="select"
+            onChange={(event) => {
+              handleChange("category", event.target.value);
+            }}
+            value={eComProducts.category}
+          >
+            <option>Running Shoes</option>
+            <option>Lifestyle</option>
+            <option>Football Shoes</option>
+            <option>Winter Boots</option>
+            <option>Sandals</option>
           </Form.Control>
         </Form.Group>
-        <Form.Group controlId="blog-content" className="mt-3">
-          <Form.Label>Blog Content</Form.Label>
-          <ReactQuill
-            value={text}
-            onChange={handleChange}
-            className="new-blog-content"
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={6}
+            onChange={(event) => {
+              handleChange("description", event.target.value);
+            }}
+            value={eComProducts.description}
           />
         </Form.Group>
         <Form.Group className="d-flex mt-3 justify-content-end">
@@ -44,6 +118,7 @@ const NewBlogPost = (props) => {
             style={{
               marginLeft: "1em",
             }}
+            onClick={submitData}
           >
             Submit
           </Button>
